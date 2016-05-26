c8a512b7ca67b84f558850d4768ce10186061700
Think <Think@Think-PC>
2016-05-26 19:32:08 +0200

    initial checkin of sales tool

---
+++ src/test/Main.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayCreeting();
+		data.display();
+	
+
+	}
+
+	private static void displayCreeting() {
+	System.out.println("Hello Happy Sales People");
+	System.out.println("this app shows sales data");
+		
+	}
+
+}
